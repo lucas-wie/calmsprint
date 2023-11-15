546cba2beff613697f043c2545aa925fafc561b9
lucas-wie <lucaswie@outlook.com>
2023-11-15 15:12:31 -0300

    project creation

---
+++ calmsprint/src/main/java/com/calmsprint/CalmsprintApplication.java
@@ -0,0 +1,13 @@
+package com.calmsprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmsprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalmsprintApplication.class, args);
+	}
+
+}
---
+++ calmsprint/src/test/java/com/calmsprint/CalmsprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calmsprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmsprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
